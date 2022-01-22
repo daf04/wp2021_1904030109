ef7f503c1ab4899634ae732fe36f1859f908c05d
daf04 <aldrichdaffa0401@gmail.com>
2022-01-22 21:26:17 +0700

    EKT 2

---
+++ EKT 2/koneksi.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'wpsmt5';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+$result = mysqli_query($conn, 'SELECT * FROM calon_mhs');
+
+// Ambil data dari tabel calon mhs
+// mysqli_fetch_row(); mengembalikan data dalam bentuk numerik
+// mysqli_fetch_assoc(); mengembalikan data dalam nama field
+// mysqli_fetch_array(); mengembalikan data dalam bentuk numerik dan nama field
+
+// while ($camaba = mysqli_fetch_row($result)){
+// var_dump($camaba);
+// }

27e48521d31aa65fee78987aa6daf8f2500769cb
daf04 <aldrichdaffa0401@gmail.com>
2022-01-22 19:09:31 +0700

    menambahkan pertemuan 4

---
+++ pertemuan 4/index.php
@@ -0,0 +1,25 @@
+<?php
+//Variable tipe data
+$nama = "Aldrich Daffa Zalfansyah";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="uts-8">
+  <title>Pertemuan 4</title>
+</head>
+
+<body>
+  <h1>Selamat Data di Mata Kuliah Web Programming</h1>
+  <h2>Selamat Datang <?php echo "$nama"; ?></h2>
+
+  <hr>
+
+  <?php
+  echo "<h2> Selamat Datang, Aldrich Daffa Zalfansyah </h2>"
+  ?>
+</body>
+
+</html>
